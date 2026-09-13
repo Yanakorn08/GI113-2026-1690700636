4cbf5b8d3740c0eb64fb1092b84f91f2150e28ba
Yanakorn08 <yanakorn.rodc@bumail.net>
2026-09-13 23:20:25 +0700

    AS1_tast1

---
+++ Assignment01/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Student ID : 1690700636
+ * Name       : Yanakorn Rodclom
+ * Section    : 129A
+ * No.        : 26
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Assignment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Slayer = "Benvolio";
+            var Weaponusing = "Sword";
+            char Gender = 'M';
+            int SmallEnemieDemage = 22;
+            var GiantsEnemieDemage = 22;
+            var BossDemage = 8;
+            float CoinPerSecond = 23.2f;
+            double CriticalDemage = 32.4;
+            bool isSlayer = true;
+            const string Weapon = "Sword";
+            
+            Console.WriteLine("Slayer Name : " + Slayer);
+            Console.WriteLine("Weapon : " + Weaponusing);
+            Console.WriteLine("Gender : " + Gender);
+            Console.WriteLine("Small Enemy Damage : " + SmallEnemieDemage);
+            Console.WriteLine("Giant Enemy Damage : " + GiantsEnemieDemage);
+            Console.WriteLine("Boss Damage : " + BossDemage);
+            Console.WriteLine("Coins Per Second : " + CoinPerSecond);
+            Console.WriteLine("Critical Damage : " + CriticalDemage);
+            Console.WriteLine("Is Slayer : " + isSlayer);
+            Console.WriteLine("Weapon : " + Weapon);
+
+        }
+    }
+}
