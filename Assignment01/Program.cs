73ac8173f90ddc44c746ff0c88c55b5b616b50ab
Yanakorn08 <yanakorn.rodc@bumail.net>
2026-09-17 23:18:56 +0700

    UD ASI01

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -12,28 +12,44 @@ namespace Assignment01
     {
         static void Main(string[] args)
         {
-            string Slayer = "Benvolio";
-            var Weaponusing = "Sword";
-            char Gender = 'M';
-            int SmallEnemieDemage = 22;
-            var GiantsEnemieDemage = 22;
-            var BossDemage = 8;
-            float CoinPerSecond = 23.2f;
-            double CriticalDemage = 32.4;
+            string slayer = "Benvolio";
+            var weaponUsing = "Sword";
+            char gender = 'M';
+            int smallEnemyDamage = 22;
+            var giantsEnemyDamage = 22;
+            var bossDamage = 8;
+            float coinPerSecond = 23.2f;
+            double criticalDemage = 32.6;
             bool isSlayer = true;
             const string Weapon = "Sword";
             
-            Console.WriteLine("Slayer Name : " + Slayer);
-            Console.WriteLine("Weapon : " + Weaponusing);
-            Console.WriteLine("Gender : " + Gender);
-            Console.WriteLine("Small Enemy Damage : " + SmallEnemieDemage);
-            Console.WriteLine("Giant Enemy Damage : " + GiantsEnemieDemage);
-            Console.WriteLine("Boss Damage : " + BossDemage);
-            Console.WriteLine("Coins Per Second : " + CoinPerSecond);
-            Console.WriteLine("Critical Damage : " + CriticalDemage);
-            Console.WriteLine("Is Slayer : " + isSlayer);
-            Console.WriteLine("Weapon : " + Weapon);
-
+            Console.WriteLine($"+-----------------------------------------------------------+");
+            Console.WriteLine($"|                                                           |");
+            Console.WriteLine($"| ___  ____     ____        _____                ____ _____ |");
+            Console.WriteLine($"|  |  |   | |   |           |      |     /|  | / |    |___| |");
+            Console.WriteLine($"|  |  |   | |   |--         |----| |    /_|  |/  |--  | |   |");
+            Console.WriteLine($"| _|_ |__/  |__ |___        _____| |__ /  |  |   |___ |  |  |");
+            Console.WriteLine($"|                                                           |");
+            Console.WriteLine($"+-----------------------------------------------------------+");
+            Console.WriteLine();
+            Console.WriteLine($"Slayer Name : {slayer}");
+            Console.WriteLine($"Weapon : {weaponUsing}");
+            Console.WriteLine($"Gender : {gender}");
+            Console.WriteLine($"Small Enemy Damage : {smallEnemyDamage}");
+            Console.WriteLine($"Giant Enemy Damage : {giantsEnemyDamage}");
+            Console.WriteLine($"Boss Damage : {bossDamage}");
+            Console.WriteLine($"Coins Per Second : {coinPerSecond}");
+            Console.WriteLine($"Critical Damage : {criticalDemage}");
+            Console.WriteLine($"Is Slayer : {isSlayer}");
+            Console.WriteLine($"Weapon : {Weapon}");
+            Console.WriteLine();
+            double giantEnemyDamageDouble = giantsEnemyDamage;
+            Console.WriteLine($"Giant Enemy Damage (Double) : {giantEnemyDamageDouble}");
+            Console.WriteLine();
+            int criticalDamageBoxBoost = (int)criticalDemage;
+            int criticalDamageSkillBoost = Convert.ToInt32(criticalDemage);
+            Console.WriteLine($"Critical Damage (Int) : {criticalDamageBoxBoost}");
+            Console.WriteLine($"Boss Damage (Int) : {criticalDamageSkillBoost}");
         }
     }
 }
